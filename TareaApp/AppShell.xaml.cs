01a3e9ad3312513281017f58dd3166fe9c3e1eed
Manuel Escobar <ignaciodeleone8@gmail.com>
2023-10-16 00:00:24 -0600

    Agregar archivos de proyecto.

---
+++ TareaApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using TareaApp.ViewModels;
+using TareaApp.Views;
+using Xamarin.Forms;
+
+namespace TareaApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}

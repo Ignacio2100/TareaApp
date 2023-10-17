962c37ddb79cd6f711a6d431e7e698d209a04ae0
Manuel Escobar <ignaciodeleone8@gmail.com>
2023-10-17 01:46:57 -0600

    Segundo Ejercicio Hecho

--- TareaApp/Views/ItemsPage.xaml.cs
+++ TareaApp/Views/ItemsPage.xaml.cs
@@ -12,21 +12,45 @@ using Xamarin.Forms.Xaml;
 
 namespace TareaApp.Views
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ItemsPage : ContentPage
     {
-        ItemsViewModel _viewModel;
-
         public ItemsPage()
         {
             InitializeComponent();
 
-            BindingContext = _viewModel = new ItemsViewModel();
+            
+        }
+        private void CalculateLoanAmount_Clicked(object sender, EventArgs e)
+        {
+            if (double.TryParse(SalaryEntry.Text, out double salary) && int.TryParse(YearsWorkedEntry.Text, out int yearsWorked))
+            {
+                double loanAmount = 0;
+
+                if (yearsWorked >= 10)
+                {
+                    loanAmount = salary * 5;
+                }
+                else if (yearsWorked >= 4)
+                {
+                    loanAmount = salary * 4;
+                }
+                else if (yearsWorked >= 2)
+                {
+                    loanAmount = salary * 2;
+                }
+                else if (yearsWorked >= 1)
+                {
+                    loanAmount = salary * 1;
+                }
+
+                LoanAmountLabel.Text = $"Monto a Prestar: {loanAmount}";
+            }
+            else
+            {
+                DisplayAlert("Error", "Ingrese valores válidos en salario y años trabajados.", "OK");
+            }
         }
 
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
-            _viewModel.OnAppearing();
-        }
     }
 }

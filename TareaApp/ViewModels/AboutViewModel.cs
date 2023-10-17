c5fed1ba01b9d14a55698645d2127ab812e5c76e
Manuel Escobar <ignaciodeleone8@gmail.com>
2023-10-16 23:07:38 -0600

    Ejercicio1 hecho

--- TareaApp/ViewModels/AboutViewModel.cs
+++ TareaApp/ViewModels/AboutViewModel.cs
@@ -7,75 +7,8 @@ namespace TareaApp.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
-        private double precio;
-        private int cantidad;
-        private double total;
-        private double pagado;
-        private double cambio;
-
-        public double Precio
-        {
-            get => precio;
-            set
-            {
-                SetProperty(ref precio, value);
-                CalculateTotal();
-            }
-        }
-
-        public int Cantidad
-        {
-            get => cantidad;
-            set
-            {
-                SetProperty(ref cantidad, value);
-                CalculateTotal();
-            }
-        }
-
-        public double Total
-        {
-            get => total;
-            set => SetProperty(ref total, value);
-        }
-
-        public double Pagado
-        {
-            get => pagado;
-            set
-            {
-                SetProperty(ref pagado, value);
-                CalculateChange();
-            }
-        }
-
-        public double Cambio
-        {
-            get => cambio;
-            set => SetProperty(ref cambio, value);
-        }
-
-        public ICommand CalculateCommand { get; private set; }
-
-        public AboutViewModel()
-        {
-            CalculateCommand = new Command(CalculateChange);
-            // Inicializa las propiedades a valores predeterminados si es necesario
-            Precio = 0;
-            Cantidad = 0;
-            Total = 0;
-            Pagado = 0;
-            Cambio = 0;
-        }
-
-        private void CalculateTotal()
-        {
-            Total = Precio * Cantidad;
-        }
-
-        private void CalculateChange()
-        {
-            Cambio = Pagado - Total;
-        }
+      
+    
+       
     }
 }
--- TareaApp/Views/AboutPage.xaml.cs
+++ TareaApp/Views/AboutPage.xaml.cs
@@ -6,12 +6,69 @@ using Xamarin.Forms.Xaml;
 
 namespace TareaApp.Views
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AboutPage : ContentPage
     {
+        double precio;
+        double cantidad;
+        double Total;
+        double pagado;
+        double Cambio;
+        double resul;
+
+        private void CalcularTotal()
+        {
+            if (!string.IsNullOrEmpty(Precio.Text) && !string.IsNullOrEmpty(Cantidad.Text))
+            {
+                precio = Convert.ToDouble(Precio.Text);
+                cantidad = Convert.ToDouble(Cantidad.Text);
+                resul = precio * cantidad;
+                lbltotal.Text = resul.ToString();
+            }
+        }
+
+        private void CalcularCambio()
+        {
+            if (!string.IsNullOrEmpty(Pagado.Text))
+            {
+                pagado = Convert.ToDouble(Pagado.Text);
+                if (pagado >= resul)
+                {
+                    Cambio = pagado - resul;
+                    lblcambio.Text = Cambio.ToString();
+                }
+                else
+                {
+                    DisplayAlert("Error", "El monto pagado debe ser igual o mayor al total.", "OK");
+                    lblcambio.Text = "0.0";
+                }
+            }
+            else
+            {
+                DisplayAlert("Error", "Ingrese el monto pagado", "OK");
+                lblcambio.Text = "0.0";
+            }
+        }
+
+        private void validar()
+        {
+            if (!string.IsNullOrEmpty(Precio.Text))
+            {
+                CalcularTotal();
+                CalcularCambio();
+            }
+            else
+            {
+                DisplayAlert("Error", "Ingrese una cantidad", "OK");
+            }
+        }
+
         public AboutPage()
         {
             InitializeComponent();
-            BindingContext = new AboutViewModel();
+            // Agregar manejadores de eventos TextChanged a los campos Precio y Cantidad
+            Precio.TextChanged += (sender, e) => CalcularTotal();
+            Cantidad.TextChanged += (sender, e) => CalcularTotal();
         }
     }
 }
